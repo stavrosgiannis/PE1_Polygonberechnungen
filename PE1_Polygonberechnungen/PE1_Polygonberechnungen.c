e2d7178e96f4fa75d7e0965d64751f6c70e5d9de
Stav ros <hackerone1@web.de>
2019-11-27 00:11:55 +0100

    00:11 27/11/2019

--- PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
+++ PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
@@ -1,20 +1,84 @@
-// PE1_Polygonberechnungen.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
+#define _CRT_SECURE_NO_WARNINGS 1
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int getArrayMin(int* x, int size) {
+	int min = x[0], i;
+	for (i = 1; i < size; i++) {
+		if (x[i] < min) {
+			min = x[i];
+		}
+	}
+	return min;
+}
+
+int getArrayLength(double _INPUT_ARRAY[]) {
+	int size;
+	size = sizeof(_INPUT_ARRAY) / sizeof(_INPUT_ARRAY[0]);
+	return size;
+}
+
+int calc_Gau�Elling_polygon_surface(double x[], double y[]) {
+	if ((x == NULL) || (y == NULL)) return 0.0;
+	int n = getArrayMin(x, getArrayLength(x));
+
+	return n;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// variables for reading the measurements from file
+	FILE* fp;
+	size_t len = 0;
+	char resp;
+
+	double x_t[20];
+	double y_t[20];
+
+	double x;
+	double y;
+
+	// open the input file "polygon.txt" for reading
+	fp = fopen("polygon.txt", "r");
+	if (fp == NULL)
+	{
+		// if file could not be opened (wrong path, not readable, ...)
+		// output a short message and immediately exit
+		printf("Eingabedatei kann nicht geoeffnet werden.\n");
+		scanf("%c", &resp);
+		exit(EXIT_FAILURE);
+	}
+
+	// print program header
+	printf("\n\nProgramm zur Berechnung eines Polygons\n");
+	printf("--------------------------------------------------------\n");
+
+	// the following loop reads a new value in every iteration
+	// until the end of the file or some invalid token is found
+	while (1)
+	{
+		len = fscanf(fp, "%lf %lf", &x, &y);
+
+		if (len == EOF)
+			break;
+		else if (len == 0) {
+			printf("Unerwartetes Zeichen in der Eingabedatei.");
+			scanf("%c", &resp);
+			exit(EXIT_FAILURE);
+		}
+
+		printf("Lese Eckpunkt: %6.2f/%6.2f\n", x, y);
+	}
+
+	// output results
+	printf("\nErgebnisse:\n");
+	printf("-----------\n\n");
+
+	// finally close the input file and clean up memory
+	fclose(fp);
+
+	// wait for user input before closing terminal
+	scanf("%c", &resp);
 }
-
-// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
