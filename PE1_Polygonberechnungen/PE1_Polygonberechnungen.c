8d61f31ed81db8ffd149f5e3f2cf23a5a7727bf4
Stav ros <hackerone1@web.de>
2019-12-07 06:35:13 +0100

    aufgaben fertig - code clean fehlt noch!

--- PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
+++ PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
@@ -7,33 +7,37 @@
 #define PATH "C:\\Users\\Stavros\\source\\repos\\PE1_Polygonberechnungen\\PE1_Polygonberechnungen\\Debug\\"
 #define ARRAY_SIZE( array ) ( sizeof( array ) / sizeof( array[0] ) )
 
+//index 1 - X; index 2 - Y
 double center_values[2];
-int anzahl_ecken = 0;
+int countPoints = 0;
+double surface = 0;
+double INPUT_HOLES[2];
 
 int getMathMinimum(double X_LENGTH, double Y_LENGTH) {
 	if (X_LENGTH > Y_LENGTH) return Y_LENGTH;
 	if (Y_LENGTH > X_LENGTH) return X_LENGTH;
 	if (Y_LENGTH == X_LENGTH) return X_LENGTH;
 }
-//ARRAY_SIZE -> sizeof - kann nicht über die Funktion mitgegeben werden
+
 double calc_GaußElling_polygon_surface(double x[], double y[]) {
 	if ((x == NULL) || (y == NULL)) return 0; // auf leere Argumente testen
-	int n = anzahl_ecken; // Anzahl der Ecken des Polygons
+	int n = countPoints; // Anzahl der Ecken des Polygons
 	if (n < 3) return 0; // ein Polygon hat mindestens drei Eckpunkte
 	double a = 0.0;
 	for (size_t i = 0; i < n; i++)
 	{
 		a += (y[i] + y[(i + 1) % n]) * (x[i] - x[(i + 1) % n]);
 	}
+	surface = abs(a / 2.0);
 	return abs(a / 2.0);
 }
 
 //Berechnung des Schwerpunkts
 double calc_geo_schwerpunkt(double x[], double y[]) {
 	if ((x == NULL) || (y == NULL)) return 0; // auf leere Argumente testen
-	int n = anzahl_ecken;
+	int n = countPoints;
 	if (n < 3) return 0; // ein Polygon hat mindestens drei Eckpunkte
-	for (size_t i = 0; i < anzahl_ecken; i++)
+	for (size_t i = 0; i < countPoints; i++)
 	{
 		center_values[0] += (x[i] + x[i + 1]) * ((x[i] * y[i + 1]) - (x[i + 1] * y[i]));
 		center_values[1] += (y[i] + y[i + 1]) * ((x[i] * y[i + 1]) - (x[i + 1] * y[i]));
@@ -94,23 +98,35 @@ int main()
 			scanf("%c", &resp);
 			exit(EXIT_FAILURE);
 		}
-		printf("Lese Eckpunkt: %6.2f/%6.2f\n", x[i], y[i]);
+		printf("Lese Eckpunkt: %6.2f[X]/%6.2f[Y]\n", x[i], y[i]);
 		i++;
-		anzahl_ecken++;
+		countPoints++;
 	}
 
 	// output results
 	printf("\nErgebnisse:\n");
-	printf("\n%f m%c", calc_GaußElling_polygon_surface(x, y), 253);
+	calc_GaußElling_polygon_surface(x, y);
+	printf("\nDie Gesamtflaeche betraegt: %fm%c", surface, 253);
 
 	calc_geo_schwerpunkt(x, y);
-	printf("\nDer Flaechenschwerpunkt liegt bei: %.6f/%.6f\n\n", center_values[0], center_values[1]);
+	printf("\nDer Flaechenschwerpunkt liegt bei: %.6f[X]/%.6f[Y]\n\n", center_values[0], center_values[1]);
 
-	printf("\n-----------\n\n");
+	printf("--------------------------------------------------------\n");
+
+	//Abfrage der Lochposition
+	printf("Bitte geben sie die gewuenschte Lochposition an(x/y):\n");
+	scanf("%lf/%lf", &INPUT_HOLES[0], &INPUT_HOLES[1]);
+	//Even-Odd-Berechnung
+	if (pnpoly(countPoints, x, y, INPUT_HOLES[0], INPUT_HOLES[1]))
+	{
+		printf("Der gegebene Punkt befindet sich innerhalb des Polygons");
+	}
+	else printf("Der gegebene Punkt befindet sich ausserhalb des Polygons");
 
 	// finally close the input file and clean up memory
 	fclose(fp);
 
 	// wait for user input before closing terminal
 	scanf("%c", &resp);
+	while (getchar() != '\n');
 }
