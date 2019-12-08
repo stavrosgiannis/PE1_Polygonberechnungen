c420ee01bfea758042592e798f99637fc8e46074
Stavros <stavros.g@web.de>
2019-12-08 11:28:59 +0100

    lapi clean up Part1

--- PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
+++ PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
@@ -13,13 +13,8 @@ int countPoints = 0;
 double surface = 0;
 double INPUT_HOLES[2];
 
-int getMathMinimum(double X_LENGTH, double Y_LENGTH) {
-	if (X_LENGTH > Y_LENGTH) return Y_LENGTH;
-	if (Y_LENGTH > X_LENGTH) return X_LENGTH;
-	if (Y_LENGTH == X_LENGTH) return X_LENGTH;
-}
 
-double calc_GauﬂElling_polygon_surface(double x[], double y[]) {
+double getGaussElling_polygon_surface(double x[], double y[]) {
 	if ((x == NULL) || (y == NULL)) return 0; // auf leere Argumente testen
 	int n = countPoints; // Anzahl der Ecken des Polygons
 	if (n < 3) return 0; // ein Polygon hat mindestens drei Eckpunkte
@@ -33,7 +28,7 @@ double calc_GauﬂElling_polygon_surface(double x[], double y[]) {
 }
 
 //Berechnung des Schwerpunkts
-double calc_geo_schwerpunkt(double x[], double y[]) {
+double getGeo_Schwerpunkt(double x[], double y[]) {
 	if ((x == NULL) || (y == NULL)) return 0; // auf leere Argumente testen
 	int n = countPoints;
 	if (n < 3) return 0; // ein Polygon hat mindestens drei Eckpunkte
@@ -43,8 +38,8 @@ double calc_geo_schwerpunkt(double x[], double y[]) {
 		center_values[1] += (y[i] + y[i + 1]) * ((x[i] * y[i + 1]) - (x[i + 1] * y[i]));
 	}
 
-	center_values[0] = abs(center_values[0] / (6 * calc_GauﬂElling_polygon_surface(x, y)));
-	center_values[1] = abs(center_values[1] / (6 * calc_GauﬂElling_polygon_surface(x, y)));
+	center_values[0] = abs(center_values[0] / (6 * getGaussElling_polygon_surface(x, y)));
+	center_values[1] = abs(center_values[1] / (6 * getGaussElling_polygon_surface(x, y)));
 }
 
 int pnpoly(int nvert, double* vertx, double* verty, double testx, double testy)
@@ -65,11 +60,11 @@ int main()
 	size_t len = 0;
 	char resp;
 
-	double x[100];
-	double y[100];
+	double INPUT_x_coordinates[100];
+	double INPUT_y_coordinates[100];
 
 	// open the input file "polygon.txt" for reading
-	fp = fopen(PATH "polygon.txt", "r");
+	fp = fopen("polygon.txt", "r");
 	if (fp == NULL)
 	{
 		// if file could not be opened (wrong path, not readable, ...)
@@ -89,7 +84,7 @@ int main()
 
 	while (1)
 	{
-		len = fscanf(fp, "%lf %lf", &x[i], &y[i]);
+		len = fscanf(fp, "%lf %lf", &INPUT_x_coordinates[i], &INPUT_y_coordinates[i]);
 
 		if (len == EOF)
 			break;
@@ -98,17 +93,17 @@ int main()
 			scanf("%c", &resp);
 			exit(EXIT_FAILURE);
 		}
-		printf("Lese Eckpunkt: %6.2f[X]/%6.2f[Y]\n", x[i], y[i]);
+		printf("Lese Eckpunkt: %6.2f[X]/%6.2f[Y]\n", INPUT_x_coordinates[i], INPUT_y_coordinates[i]);
 		i++;
 		countPoints++;
 	}
 
 	// output results
 	printf("\nErgebnisse:\n");
-	calc_GauﬂElling_polygon_surface(x, y);
+	getGaussElling_polygon_surface(INPUT_x_coordinates, INPUT_y_coordinates);
 	printf("\nDie Gesamtflaeche betraegt: %fm%c", surface, 253);
 
-	calc_geo_schwerpunkt(x, y);
+	getGeo_Schwerpunkt(INPUT_x_coordinates, INPUT_y_coordinates);
 	printf("\nDer Flaechenschwerpunkt liegt bei: %.6f[X]/%.6f[Y]\n\n", center_values[0], center_values[1]);
 
 	printf("--------------------------------------------------------\n");
@@ -117,7 +112,7 @@ int main()
 	printf("Bitte geben sie die gewuenschte Lochposition an(x/y):\n");
 	scanf("%lf/%lf", &INPUT_HOLES[0], &INPUT_HOLES[1]);
 	//Even-Odd-Berechnung
-	if (pnpoly(countPoints, x, y, INPUT_HOLES[0], INPUT_HOLES[1]))
+	if (pnpoly(countPoints, INPUT_x_coordinates, INPUT_y_coordinates, INPUT_HOLES[0], INPUT_HOLES[1]))
 	{
 		printf("Der gegebene Punkt befindet sich innerhalb des Polygons");
 	}
