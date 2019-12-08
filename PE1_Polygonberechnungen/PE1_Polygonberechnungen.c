e51db6abc7f42892cb8a7391b37f7f4095c3a036
Stav ros <hackerone1@web.de>
2019-12-08 18:01:10 +0100

    18:00 8/12/2019

--- PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
+++ PE1_Polygonberechnungen/PE1_Polygonberechnungen.c
@@ -5,17 +5,16 @@
 #include <math.h>
 #include <string.h>
 
-char dirPATH[];
 #define ARRAY_SIZE( array ) ( sizeof( array ) / sizeof( array[0] ) )
 
 //index 1 - X; index 2 - Y
 double center_values[2];
 int countPoints = 0;
-double surface = 0;
 double INPUT_HOLES[2];
 
+char cwd[100];
 
-double getGaussElling_polygon_surface(double x[], double y[]) {
+double get_polygon_surface(double x[], double y[]) {
 	if ((x == NULL) || (y == NULL)) return 0; // auf leere Argumente testen
 	int n = countPoints; // Anzahl der Ecken des Polygons
 	if (n < 3) return 0; // ein Polygon hat mindestens drei Eckpunkte
@@ -24,23 +23,23 @@ double getGaussElling_polygon_surface(double x[], double y[]) {
 	{
 		a += (y[i] + y[(i + 1) % n]) * (x[i] - x[(i + 1) % n]);
 	}
-	surface = abs(a / 2.0);
-	return abs(a / 2.0);
+	return (a / 2.0);
 }
 
 //Berechnung des Schwerpunkts
 double getGeo_Schwerpunkt(double x[], double y[]) {
-	if ((x == NULL) || (y == NULL)) return 0; // auf leere Argumente testen
+	if ((x == NULL) || (y == NULL)) return 1; // auf leere Argumente testen
 	int n = countPoints;
-	if (n < 3) return 0; // ein Polygon hat mindestens drei Eckpunkte
+	if (n < 3) return 1; // ein Polygon hat mindestens drei Eckpunkte
 	for (size_t i = 0; i < countPoints; i++)
 	{
 		center_values[0] += (x[i] + x[i + 1]) * ((x[i] * y[i + 1]) - (x[i + 1] * y[i]));
 		center_values[1] += (y[i] + y[i + 1]) * ((x[i] * y[i + 1]) - (x[i + 1] * y[i]));
 	}
 
-	center_values[0] = abs(center_values[0] / (6 * getGaussElling_polygon_surface(x, y)));
-	center_values[1] = abs(center_values[1] / (6 * getGaussElling_polygon_surface(x, y)));
+	center_values[0] = center_values[0] / (6 * get_polygon_surface(x, y));
+	center_values[1] = center_values[1] / (6 * get_polygon_surface(x, y));
+	return 0;
 }
 
 int pnpoly(int nvert, double* vertx, double* verty, double testx, double testy)
@@ -54,20 +53,18 @@ int pnpoly(int nvert, double* vertx, double* verty, double testx, double testy)
 	return c;
 }
 
+char getDIR() {
+#include <io.h>
 
-int getDIR(){
-	#include <unistd.h>
+	char* getcwd(char* buf, size_t size);
 
-	char *getcwd(char *buf, size_t size);
-	char cwd[100];
-
-	if (getcwd(cwd, sizeof(cwd)) != NULL){
-		printf("Current working directory:\n%s", cwd);
-		dirPATH == cwd;
-	}else {
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		printf("Current working directory: %s\n", cwd);
+		return cwd;
+	}
+	else {
 		printf("getcwd() error");
-		dirPATH == "";
-	return 1;
+		return "";
 	}
 }
 
@@ -81,12 +78,10 @@ int main()
 	double INPUT_x_coordinates[100];
 	double INPUT_y_coordinates[100];
 
-	getDIR();
-	
-
 	// open the input file "polygon.txt" for reading
-	//printf("%s", strcat(dirPATH, "/polygon.txt"));
-	fp = fopen(strcat(dirPATH, "/polygon.txt"), "r");
+	getDIR();
+	printf("Full Path: %s\n", strcat(cwd, "\\polygon.txt"));
+	fp = fopen(cwd, "r");
 	if (fp == NULL)
 	{
 		// if file could not be opened (wrong path, not readable, ...)
@@ -115,7 +110,7 @@ int main()
 			scanf("%c", &resp);
 			exit(EXIT_FAILURE);
 		}
-		if (len > 100){
+		if (len > 100) {
 			printf("Es sind nicht mehr als 100 Eckpunkte erlaubt!");
 			exit(EXIT_FAILURE);
 		}
@@ -125,9 +120,9 @@ int main()
 	}
 
 	// output results
-	printf("\nErgebnisse:\n");
-	getGaussElling_polygon_surface(INPUT_x_coordinates, INPUT_y_coordinates);
-	printf("\nDie Gesamtflaeche betraegt: %fm%c", surface, 253);
+	printf("\nErgebnisse:");
+	get_polygon_surface(INPUT_x_coordinates, INPUT_y_coordinates);
+	printf("\nDie Gesamtflaeche betraegt: %fm%c", get_polygon_surface(INPUT_x_coordinates, INPUT_y_coordinates), 253);
 
 	getGeo_Schwerpunkt(INPUT_x_coordinates, INPUT_y_coordinates);
 	printf("\nDer Flaechenschwerpunkt liegt bei: %.6f[X]/%.6f[Y]\n\n", center_values[0], center_values[1]);
